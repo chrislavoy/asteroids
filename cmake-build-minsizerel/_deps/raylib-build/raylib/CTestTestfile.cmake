ee0d155356d3c265f04547a8c2387cd474886fe4
Chris LaVoy <chrislavoy@users.noreply.github.com>
2021-07-20 23:22:57 -0700

    Removed build folders

--- cmake-build-minsizerel/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Code/asteroids/cmake-build-minsizerel/_deps/raylib-src/src
-# Build directory: C:/Code/asteroids/cmake-build-minsizerel/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
